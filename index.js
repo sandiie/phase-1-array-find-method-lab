a19cb31c1fa4f877d552b6a004f4ff28c01ea655
+ <+>
2024-03-27 17:14:18 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin (record) {
+    const win =record.find((game) => game.result==="W" &&game.team
+    ==="Denver Broncos");
+    if(win){
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
+function superbowlWin(records){
+    const winRecord = records.find(element => element.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+
+console.log(superbowlWin(record));
